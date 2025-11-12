e7c1de2e08b4f16783ba2f279639ebeda5276861
NadirBakhsh <nadir4k2010@gmail.com>
2025-11-12 11:44:18 +0500

    refactor: clean up main.ts by removing unnecessary semicolons and updating global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,22 @@
-import { NestFactory } from "@nestjs/core";
+import { NestFactory } from "@nestjs/core"
 
-import { AppModule } from "./app.module";
-import { setupSwagger } from "./swagger/swagger.config";
+import { AppModule } from "./app.module"
+import { setupSwagger } from "./swagger/swagger.config"
+import { VersioningType } from "@nestjs/common"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
   // Setup global prefix
-  app.setGlobalPrefix("api/v1");
+  app.setGlobalPrefix("api/")
 
   // Swagger Setup
-  setupSwagger(app);
+  setupSwagger(app)
 
-  await app.listen(process.env.PORT ?? 5000);
+  await app.listen(process.env.PORT ?? 5000)
 }
 
 bootstrap().catch((error) => {
-  console.error("Application failed to start:", error);
-  process.exit(1);
-});
+  console.error("Application failed to start:", error)
+  process.exit(1)
+})
