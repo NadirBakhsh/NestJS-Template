455614ffc69d6812adb47ce7e9307c1c3a2d118f
NadirBakhsh <nadir4k2010@gmail.com>
2025-12-30 23:49:32 +0500

    feat(database): integrate PostgreSQL with TypeORM and add environment validation
    - Updated AppConfigurationModule to include database configuration and validation.
    - Enhanced README.md with detailed PostgreSQL setup instructions and best practices.
    - Created database.config.ts for centralized database configuration management.
    - Implemented database.module.ts to initialize TypeORM with PostgreSQL settings.
    - Added db.env.validation.ts for environment variable validation using Joi.

--- src/configurations/app.config.module.ts
+++ src/configurations/app.config.module.ts
@@ -2,17 +2,20 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import environmentSchema from "./environment.validation";
 import serverConfig from "./server.config";
+import dbEnvValidationSchema from "src/database/postgresql/db.env.validation";
+import databaseConfig from "src/database/postgresql/database.config";
+import { DatabaseModule } from "src/database/postgresql/database.module";
 
 const ENV = process.env.NODE_ENV || "development";
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: !ENV ? ".env.development" : `.env.${ENV}`,
-      load: [serverConfig],
-      validationSchema: environmentSchema,
+      load: [serverConfig, databaseConfig],
+      validationSchema: environmentSchema.concat(dbEnvValidationSchema),
     }),
+    DatabaseModule,
   ],
 })
 export class AppConfigurationModule {}
