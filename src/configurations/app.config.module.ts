627358781ff9609cb1e0cf444c10331adf5f6fa5
NadirBakhsh <nadir4k2010@gmail.com>
2025-12-31 19:28:58 +0500

    feat(database): integrate MySQL configuration and validation
    - Added DatabaseModule for MySQL integration using TypeORM.
    - Created database.config.ts for database configuration using NestJS ConfigService.
    - Implemented db.env.validation.ts for environment variable validation with Joi.
    - Updated app.config.module.ts to include DatabaseModule and load database configuration.
    - Enhanced README.md with detailed setup instructions and common issues for MySQL configuration.

--- src/configurations/app.config.module.ts
+++ src/configurations/app.config.module.ts
@@ -2,6 +2,9 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import environmentSchema from "./environment.validation";
 import serverConfig from "./server.config";
+import { DatabaseModule } from "../database/mysql/database.module";
+import dbEnvValidationSchema from "src/database/mysql/db.env.validation";
+import databaseConfig from "src/database/mysql/database.config";
 
 const ENV = process.env.NODE_ENV || "development";
 
@@ -10,9 +13,10 @@ const ENV = process.env.NODE_ENV || "development";
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: !ENV ? ".env.development" : `.env.${ENV}`,
-      load: [serverConfig],
-      validationSchema: environmentSchema,
+      load: [serverConfig, databaseConfig],
+      validationSchema: environmentSchema.concat(dbEnvValidationSchema),
     }),
+    DatabaseModule,
   ],
 })
 export class AppConfigurationModule {}
